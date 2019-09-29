ec60ee07c82eedd98147e1939fde3124f782e8d3
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-09-29 10:55:43 +0200

    Added one-time browser init

--- src/page-loader/page-loader.service.ts
+++ src/page-loader/page-loader.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PageLoader } from './interfaces/page-loader.interface';
 import * as puppeteer from 'puppeteer';
-import { Page } from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
 
 @Injectable()
 export class PageLoaderService implements PageLoader
 {
+    protected browser: Browser;
+
     public async load( url: string ): Promise<Page>
     {
-        const browser = await puppeteer.launch( { headless: false } );
-        const page = await browser.newPage();
+        if ( !this.browser ) {
+            await this.init();
+        }
+
+        const page = await this.browser.newPage();
 
         await page.setViewport( { width: 1920, height: 926 } );
         await page.goto( url );
 
         return page;
+    }
+
+    protected async init(): Promise<void>
+    {
+        this.browser = await puppeteer.launch( { headless: false } );
     }
 
 }
