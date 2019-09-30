1a6fb2b5d4e34e47781e9b52870929113816648b
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-09-30 19:48:03 +0200

    Added mongoose connection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO Caching
 async function bootstrap()
 {
     const app = await NestFactory.create( AppModule );
