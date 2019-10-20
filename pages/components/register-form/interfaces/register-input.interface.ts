ad12bfe965e08221bbe94fe5f7fd30ca1e1e542b
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-10-20 12:59:43 +0200

    Added formik wrapper

---
+++ pages/components/register-form/interfaces/register-input.interface.ts
@@ -0,0 +1,5 @@
+export default interface RegisterInput
+{
+    email: string;
+    password: string;
+}
--- pages/components/register-form/register-form.tsx
+++ pages/components/register-form/register-form.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { FC } from 'react';
+import { FormikProps, withFormik } from 'formik';
+import RegisterFormProps from './interfaces/register-form-props.interface';
+import RegisterInput from './interfaces/register-input.interface';
 
-const RegisterForm: FC = () =>
+const RegisterForm: FC<FormikProps<RegisterInput> & RegisterInput> = () =>
 {
     return (
         <div>
@@ -10,4 +13,15 @@ const RegisterForm: FC = () =>
     );
 };
 
-export default RegisterForm;
+const formikWrapper = withFormik<RegisterFormProps, RegisterInput>( {
+    mapPropsToValues: () => ( {
+        password: '',
+        email:    '',
+    } ),
+    handleSubmit:     async ( { email, password }, { setSubmitting, setError } ) =>
+                      {
+
+                      },
+} );
+
+export default formikWrapper( RegisterForm );
