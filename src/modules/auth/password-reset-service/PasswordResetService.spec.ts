0587bcbeee76f7d78a598cce7dc21e005100eb80
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-10-26 21:43:30 +0200

    Updated tests

--- src/modules/auth/password-reset-service/PasswordResetService.spec.ts
+++ src/modules/auth/password-reset-service/PasswordResetService.spec.ts
@@ -5,6 +5,15 @@ import { ConfigModule } from '../../config/ConfigModule';
 import PasswordResetService from './PasswordResetService';
 import MockModel from '../../../test/mocks/models/MockModel';
 import { AuthService } from '../auth-service/AuthService';
+import User from '../../users/types/User';
+import PasswordResetDocument from '../types/PasswordResetDocument';
+import UserDocument from '../../users/types/UserDocument';
+import { v4 } from 'uuid';
+import { Schema } from 'mongoose';
+
+jest.mock( 'uuid', () => ( {
+    v4: () => 'test',
+} ) );
 
 describe( 'PasswordResetService', () =>
 {
@@ -35,5 +44,24 @@ describe( 'PasswordResetService', () =>
     it( 'should be defined', () =>
     {
         expect( service ).toBeDefined();
+    } );
+
+    it( 'resetPassword', async () =>
+    {
+        const user: Partial<UserDocument> = {
+            _id:  new Schema.Types.ObjectId( '1' ),
+            save: jest.fn(),
+        };
+        const passwordResetDoc: Partial<PasswordResetDocument> = {
+            user: user as UserDocument,
+        };
+
+        jest
+            .spyOn( service, 'findByToken' )
+            .mockReturnValue( Promise.resolve( passwordResetDoc as PasswordResetDocument ) );
+
+        await service.resetPassword( v4() );
+
+        expect( user.password ).toEqual( 'test' );
     } );
 } );
