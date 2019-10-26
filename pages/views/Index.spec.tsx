81b3f0d816cfb6c3827005427dd351d537edb13d
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-10-26 13:56:42 +0200

    Updated tests

--- pages/views/Index.spec.tsx
+++ pages/views/Index.spec.tsx
@@ -1,11 +1,47 @@
 import { mount } from 'enzyme';
 import Index from './Index';
 import * as React from 'react';
+import MockAdapter from 'axios-mock-adapter';
+import client from '../http/client';
+import { wait } from '../../src/utils/timeout';
+import redirect from '../http/redirect';
+import { act } from 'react-dom/test-utils';
+
+jest.mock( '../http/redirect', () => ( {
+    default: jest.fn(),
+} ) );
 
 describe( 'Index component', () =>
 {
-    it( 'Renders without crashing', () =>
+    let mockAdapter: MockAdapter;
+
+    beforeEach( () =>
+    {
+        jest.resetModules();
+
+        mockAdapter = new MockAdapter( client );
+    } );
+
+    it( 'renders without crashing', () =>
     {
         mount( <Index/> );
+    } );
+
+    it( 'should redirect to login page if user is not logged in', async () =>
+    {
+        mockAdapter.onPost( '/users/me' ).replyOnce( 200, {
+            result: null,
+        } );
+
+        const mockRedirect = redirect as jest.Mock;
+
+        await act( async () =>
+        {
+            mount( <Index/> );
+
+            await wait( 500 );
+        } );
+
+        expect( mockRedirect ).toBeCalledWith( '/auth/login' );
     } );
 } );
--- pages/views/Index.tsx
+++ pages/views/Index.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { NextPage, NextPageContext } from 'next';
 import AppHead from '../components/app-head/AppHead';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useCurrentUser from '../hooks/useCurrentUser';
+import { isEmpty } from 'lodash';
+import redirect from '../http/redirect';
 
 const Index: NextPage<any> = ( { title = 'Foods scrapper' } ) =>
 {
+    const [ { loading, data, error } ] = useCurrentUser();
+
+    useEffect( () =>
+    {
+        if ( !loading ) {
+            if ( isEmpty( data ) || !isEmpty( error ) ) {
+                redirect( '/auth/login' );
+            }
+        }
+    }, [ loading, data, error ] );
+
     return (
         <>
             <AppHead title={ title }/>
