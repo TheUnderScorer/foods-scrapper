02d4cda548a0fe4cccdeace0d6f9fe9506bbe6c1
Przemysław Żydek <przemyslawzydek@gmail.com>
2019-11-04 08:18:25 +0100

    Added formik wrapper

--- pages/components/scrapper-form/ScrapperForm.tsx
+++ pages/components/scrapper-form/ScrapperForm.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import { FC } from 'react';
+import { FormikProps, withFormik } from 'formik';
+import ScrapperFormProps from './types/ScrapperFormProps';
+import GetFoodsDto from '../../../src/modules/foods/dto/GetFoodsDto';
 
-const ScrapperForm: FC = () =>
+const ScrapperForm: FC<FormikProps<GetFoodsDto> & ScrapperFormProps> = ( props ) =>
 {
     return null;
 };
 
-export default ScrapperForm;
+const formikWrapper = withFormik<ScrapperFormProps, GetFoodsDto>( {
+    mapPropsToValues: ( { defaults = {} } ) => ({
+        services: defaults.services ? defaults.services : [],
+        keywords: defaults.keywords ? defaults.keywords : [],
+        location: defaults.location ? defaults.location : '',
+    }),
+    handleSubmit: async () =>
+    {
+    },
+} );
+
+export default formikWrapper( ScrapperForm );
---
+++ pages/components/scrapper-form/types/ScrapperFormProps.ts
@@ -0,0 +1,6 @@
+import GetFoodsDto from '../../../../src/modules/foods/dto/GetFoodsDto';
+
+export default interface ScrapperFormProps
+{
+    defaults?: GetFoodsDto;
+}
